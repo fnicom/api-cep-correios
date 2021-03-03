3afcd41fa8b6daa6bbf8e158a313312cd16c1c6d
fnicomedes <fnicomedes@gmail.com>
2021-03-03 14:07:13 -0300

    html pronto

---
+++ js/api.js
@@ -0,0 +1,9 @@
+
+let botao = document.querySelector('button');
+
+botao.addEventListener('click', function() {
+    
+    let cep = document.querySelector('#cep').value;
+
+    console.log(cep);
+});
